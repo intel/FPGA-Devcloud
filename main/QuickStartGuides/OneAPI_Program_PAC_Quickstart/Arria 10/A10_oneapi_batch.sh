406d8f8a8d243adf5d0fe82ba09e9da95ca3db20
damarisrenteria <59750149+damarisrenteria@users.noreply.github.com>
2020-05-29 10:31:22 -0700

    Copy vector-add files
    Wget all url files and add them to devcloud directory

--- main/QuickStartGuides/OneAPI_Program_PAC_Quickstart/Arria 10/A10_oneapi_batch.sh
+++ main/QuickStartGuides/OneAPI_Program_PAC_Quickstart/Arria 10/A10_oneapi_batch.sh
@@ -1,13 +1,19 @@
 # Initial Setup
 source /data/intel_fpga/devcloudLoginToolSetup.sh
 tools_setup -t A10OAPI
-mkdir A10_ONEAPI
+#mkdir A10_ONEAPI
+mkdir -r A10_ONEAPI/vector-add
+
 # Copy Over sample design
-cd A10_ONEAPI
-mkdir vector-add
+cd ~/A10_ONEAPI/vector-add
+wget https://raw.github/download-file-list.txt
+wget -i download-file-list.txt
 
 # Running project in Emulation mode
-cd ~/A10_ONEAPI/vector-add
+#cd ~/A10_ONEAPI/vector-add
 make run_emu -f Makefile.fpga
+error_check()
+
 # Running project in FPGA Hardware Mode
 make run_hw -f Makefile.fpga
+error_check()
