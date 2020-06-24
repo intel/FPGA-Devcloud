4e1f940b8cfbf1b2b712af33ed307ccba30d757f
damarisrenteria <59750149+damarisrenteria@users.noreply.github.com>
2020-06-24 10:01:54 -0600

    Included manual commands needed for PACSign
    Manually need to do PACSign command to convert to unsigned.aocx

--- main/QuickStartGuides/OpenCL_Program_PAC_Quickstart/Arria 10/A10_v1.2.1_opencl_batch.sh
+++ main/QuickStartGuides/OpenCL_Program_PAC_Quickstart/Arria 10/A10_v1.2.1_opencl_batch.sh
@@ -1,3 +1,18 @@
+# Copyright 2020 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions
+# of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 #############################################################################################################
 # The following flow assumes A10_OPENCL_AFU directory doesn't exist and sample design hasn't been copied over
 # Arria 10 Devstack version 1.2.1
@@ -39,11 +54,18 @@ error_check
 # Get device name
 aocl diagnose
 error_check
+
 # Converting to an unsigned .aocx file
-cd bin
-printf "Y\nY\n" | source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i hello_world_fpga.aocx -r NULL -k NULL -o hello_world_fpga_unsigned.aocx
-error_check
-# Programmming PAC Card
-aocl program acl0 hello_world_fpga_unsigned.aocx
-./host
-error_check
+##############################################################################################
+##### In development. For now please run the following manually to successfully convert to an
+##### unsigned .aocx file, program the PAC card, and run the host code.
+#devcloud_login -I A10PAC 1.2.1
+#tools_setup -t A10DS
+#cd A10_OPENCL_AFU/v1.2.1/hello_world/bin
+#source $AOCL_BOARD_PACKAGE_ROOT/linux64/libexec/sign_aocx.sh -H openssl_manager -i hello_world_fpga.aocx -r NULL -k NULL -o hello_world_fpga_unsigned.aocx
+##### Type Y to the following to accept an unsigned bitstream
+#       No root key specified. Generate unsigned bitstream? Y = yes, N = no: Y
+#       No CSK specified. Generate unsigned bitstream? Y = yes, N = no: Y
+##### Programmming PAC Card
+#aocl program acl0 hello_world_fpga_unsigned.aocx
+#./host
