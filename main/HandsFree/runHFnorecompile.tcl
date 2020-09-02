27fc1016a2a3b8c73844f6cc28f7944240bf00f6
Tyler Sheaves <tyler.sheaves@intel.com>
2020-09-02 07:52:37 -0700

    Delete runHFnorecompile.tcl

--- main/HandsFree/runHFnorecompile.tcl
+++
@@ -1,4 +0,0 @@
-if {[catch {set a [open |[list quartus_sh --script ../tcl/main.tcl] r+]}] != 0 } {
-	if {[catch {cd ../; set a [open |[list quartus_sh --script ../../tcl/main.tcl] r+]}] != 0 }
-		post_message "Could not find main.tcl using relative path!"
-}
