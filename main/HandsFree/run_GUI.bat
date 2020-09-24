1c5dcb90ebf15967d2116a9d6fcc82e71a99b9f9
tsheaves <tyler.sheaves@intel.com>
2020-09-24 11:01:53 -0700

    Updated Readme, setup script, and DE10-Lite Remote Console

--- main/HandsFree/run_GUI.bat
+++ main/HandsFree/run_GUI.bat
@@ -1,5 +1,5 @@
-set path_to_launch=C:/Users/tsheaves/Documents/RemoteConsoleDE10Lite/Source/main.tcl
-set quartus_bin=C:/intelFPGA_lite/19.1/quartus/bin64
+set path_to_launch=<extract_path>/RemoteConsole<devkit>/Source/main.tcl
+set quartus_bin=<quartus_install_dir>/intelFPGA_lite/<version number>/quartus/bin64
 START /min /WAIT  C:/Windows/System32/taskkill /IM "system-console.exe" /F
 START /min /WAIT C:/Windows/System32/taskkill /IM "quartus_sh.exe" /F
 START /min %quartus_bin%/quartus_sh --script "%path_to_launch%"
